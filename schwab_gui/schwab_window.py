2f3d83a6c4ba6fb6528aab562fa148498dc4eda1
Jeff Taylor <jeffta1080@gmail.com>
2025-07-09 17:02:01 -0400

    Changed layout alignment of Schwab Window

--- schwab_gui/schwab_window.py
+++ schwab_gui/schwab_window.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QHBoxLayout
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QHBoxLayout, QSizePolicy
 
 from mongodb_connection.mongo_client import initialize_mongo_client
 from schwab_gui.preview_order_history import PreviewOrderHistoryWidget
@@ -28,18 +29,21 @@ class MainSchwabWindow(QMainWindow):
 
         # widget sizing
         stock_data_widget.setMinimumSize(300, 270)
-        preview_order_history.setMinimumSize(840, 400)
-        price_history_graph.setMinimumSize(600, 400)
-        self.adjustSize()
+        order_entry_widget.setMinimumSize(370, 300)
+        preview_order_history.setMinimumSize(840, 200)
+        price_history_graph.setMinimumSize(600, 850)
+
+        order_entry_widget.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
         # updates preview_order table when new order is created
         order_entry_widget.order_created.connect(preview_order_history.populate_order_history_table)
 
         # layout
         layout = QVBoxLayout()
-        layout.addWidget(trading_app_selector_widget)
-        layout.addWidget(stock_data_widget)
-        layout.addWidget(order_entry_widget)
+        layout.addWidget(trading_app_selector_widget, alignment=Qt.AlignmentFlag.AlignTop)
+        layout.addWidget(stock_data_widget, alignment=Qt.AlignmentFlag.AlignTop)
+        layout.addWidget(order_entry_widget, alignment=Qt.AlignmentFlag.AlignTop)
+        layout.addStretch()
 
         # horizontal layout
         horizontal_layout = QHBoxLayout()
@@ -48,8 +52,8 @@ class MainSchwabWindow(QMainWindow):
         horizontal_layout.addWidget(price_history_graph)
 
         layout_wrapper = QVBoxLayout()
-        layout_wrapper.addLayout(horizontal_layout)
-        layout_wrapper.addWidget(preview_order_history)
+        layout_wrapper.addLayout(horizontal_layout, stretch=3)
+        layout_wrapper.addWidget(preview_order_history, stretch=1)
 
         container = QWidget()
         container.setLayout(layout_wrapper)
